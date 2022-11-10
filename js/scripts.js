452e4634c422e9bcfe09f3ea48a110bd71ee993b
djimenezchocron <djimenezchocron@hotmail.com>
2022-11-10 22:05:14 +0100

    Práctica formulario con script terminado

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
 const formElement = document.getElementById('form');
-const cvcElement = document.getElementById('cvc');
-const cardCvcElement = document.getElementById('card-cvc');
-
+const cardNameElement = document.getElementById('card-name');
+const nameElement = document.getElementById('name');
 const cardNumberElement = document.getElementById('card-number');
 const numberElement = document.getElementById('number');
-
+const cardExpElement = document.getElementById('card-exp');
+const expElement = document.getElementById('exp');
+const cardCvcElement = document.getElementById('card-cvc');
+const cvcElement = document.getElementById('cvc');
 
 formElement.addEventListener('keyup', () => {
-    if (cvcElement.value !== '') {
-        cardCvcElement.textContent = cvcElement.value
+    if (nameElement.value !== '') {
+        cardNameElement.textContent = nameElement.value
     } else {
-        cardCvcElement.textContent = '000'
+        cardNameElement.textContent = 'JANE APPLESEED'
     }
     if (numberElement.value !== '') {
         cardNumberElement.textContent = numberElement.value
     } else {
         cardNumberElement.textContent = '0000 0000 0000 0000'
+    }
+    if (expElement.value !== '') {
+        cardExpElement.textContent = expElement.value
+    } else {
+        cardExpElement.textContent = "00/00"
+    }
+    if (cvcElement.value !== '') {
+        cardCvcElement.textContent = cvcElement.value
+    } else {
+        cardCvcElement.textContent = '000'
     }
 })
